41143f3f0ee9e8eb8d61010d6f94bc0735073905
qiang <qiang@qiangdeMac-mini.local>
2026-09-10 14:37:25 +0800

    feat: consume versioned truth handoff

--- runner.ts
+++ runner.ts
@@ -1,5 +1,5 @@
-import { analyzeToken } from "./src/index.js";
+import { analyzeToken } from "./src/index.js"; import { buildAnalysisFromTruth } from "./src/truth.js"; import { readFile } from "node:fs/promises";
 const address = process.argv[2];
 if (!address || !/^0x[a-fA-F0-9]{40}$/.test(address)) throw new Error("INVALID_EVM_ADDRESS");
-const artifact = await analyzeToken(address);
+const artifact = process.argv[3] ? buildAnalysisFromTruth(JSON.parse(await readFile(process.argv[3], "utf8"))) : await analyzeToken(address);
 console.log(JSON.stringify(artifact, null, 2));
